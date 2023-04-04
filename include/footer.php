5d1bf1dbac1ae0b6776c31cba4f59e338bd0d0be
alirezakh74 <alirezakhodabande74@gmail.com>
2023-04-04 19:53:34 +0330

    add designer name to footer

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,7 @@
         <footer>
             <p><address>آدرس: شیراز فرگاز</address></p>
             <p>&nbsp;تمام حقوق محفوظ است &copy; 1402</p>
+            <p>طراح: علیرضا خدابنده</p>
         </footer>
     </div>
     <script src="js/app.js"></script>
