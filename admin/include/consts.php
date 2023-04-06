dcaa74981cbf03dfaf89672403f8fe0815138225
alirezakh74 <alirezakhodabande74@gmail.com>
2023-04-06 20:03:43 +0330

    consts

---
+++ admin/include/consts.php
@@ -0,0 +1,9 @@
+<?php
+
+// consts and configurations
+define("DSN", "mysql:host=localhost;dbname:MyBlog;charset:UTF8;");
+define("DATABASE_NAME", "myblog");
+define("DB_USER_NAME", "root");
+define("DB_PASS", "");
+
+?>
